c563d5485ec9d86230aa822f3e16553e64e9f790
Shoaib Burq <saburq@gmail.com>
2019-02-27 08:34:20 +0500

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
 const projects = [
   {
-    className: "City Atlas (Australia)",
+    className: "click",
     summary:
       "The City Atlas is a project exploring spatial patterns across metropolitan areas",
     description:
@@ -10,26 +10,77 @@ const projects = [
     },
     slug: "cityatlas",
     title: "City Atlas",
-    // cover: require('./cityatlas.png'),
+    cover: require("./cityatlas.png"),
+    theme: {
+      background: "#00a5d1"
+      // light: true // Invert text color.
+    }
+  },
+  {
+    className: "kindyNow",
+    summary:
+      "Geospatial Land Use & Planning tool based on machine learning models",
+    description:
+      "As part of Land Use and Infrastructure Plan (LUIP) Digital Model, Open Space Modelling and Social Infrastructure Modelling aims to verify whether the municipality’s existing and proposed open space access and social infrastructure can meet the current and forthcoming demand created by population growth out to 2036. Through comprehensive internal research and engagement, a baseline of public, community and private sector facilities data was injected into the model.",
+    links: {
+      website: "http://vimeo.com/320035818"
+    },
+    slug: "landuse",
+    title: "City Analytics",
+    cover: require("./cityanalytics.png"),
+    theme: {
+      background: "#F7F7F7",
+      light: true // Invert text color.
+    }
+  },
+  {
+    className: "click",
+    summary:
+      "Public access a range of information on different population groups sourced from the national Census of Population and Housing",
+    description:
+      "The Office of Multicultural Interests developed this website so that Members of Parliament, State Government agencies, local governments, non-government organisations (NGOs), community groups, students and other members of the public, would be able to access a range of information on different population groups sourced from the national Census of Population and Housing. Information is available for key demographic, cultural and socioeconomic characteristics of people living across the State. It profiles these characteristics for all Western Australian electoral divisions and local government areas.",
+    links: {
+      // website: "http://vimeo.com/320035818"
+      website: "https://geografia.com.au/dlgsc/"
+    },
+    slug: "dlgsc",
+    title: "DLGSC Cultural Atlas of Western Australia",
+    cover: require("./dlgsc.png"),
+    theme: {
+      background: "#a77e60"
+      // light: true // Invert text color.
+    }
+  },
+  {
+    className: "kindyNow",
+    summary: "Cloud Based Geospatial Database as a Service",
+    description:
+      "SpacialDB, is a backend (think database) for Web and Mobile apps with a strong emphasis on Geospatial functionality. We provide developers with an easy to use API to quickly import, query and update their data without needing to worry about the backend management, maintenance and scaling. The service makes it really easy for developers to build advanced Geospatial applications directly via their iPhone, Android or WP7 SDKs. SpacialDB also integrates with existing cloud hosting services such as Heroku, Engine Yard, Joyent and Rackspace for Web apps. In 2015 it was acquired by QGISCloud",
+    links: {
+      website: "https://qgiscloud.com/"
+    },
+    slug: "spacialdb",
+    title: "SpacialDB",
+    cover: require("./spacialdb.jpg"),
     theme: {
       background: "#f4f5fa",
       light: true // Invert text color.
     }
   },
   {
-    className: "Land Use Infrastucture Model",
+    className: "click",
     summary:
-      "Geospatial Land Use & Planning tool based on machine learning models",
+      "UNLOCK YOUR ECONOMY: Track event impact, economic growth, online, visitor and escape expenditure",
     description:
-      "As part of Land Use and Infrastructure Plan (LUIP) Digital Model, Open Space Modelling and Social Infrastructure Modelling aims to verify whether the municipality’s existing and proposed open space access and social infrastructure can meet the current and forthcoming demand created by population growth out to 2036. Through comprehensive internal research and engagement, a baseline of public, community and private sector facilities data was injected into the model.",
+      "Spendmapp is an online application that allows users to view expenditure activity in and from their region. It does this by taking bank transaction (EFTPOS) data and applying a proprietary data transformation to capture virtually all economic activity within a region.",
     links: {
-      website: "https://geografia.com.au/project-city-analytics"
+      website: "http://spendmapp.com.au/"
     },
-    slug: "landuse",
-    title: "Land Use Infrastucture Model",
-    // cover: require('./cityatlas.png'),
+    slug: "spendmapp",
+    title: "Spendmapp",
+    cover: require("./spendmapp.png"),
     theme: {
-      background: "#f4f5fa",
+      background: "#bdf1ff",
       light: true // Invert text color.
     }
   }
